dac1a35b505f0720454baad3deecc96f838a263b
Sany Liew <sany.liew@cention.se>
2015-06-12 17:09:50 +0800

    initial part of import project

--- hello.go
+++ hello.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sanylcs/example/stringutil"
+)
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("\n!oG , olleH"))
 }
 
